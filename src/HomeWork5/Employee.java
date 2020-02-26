3091f70450f6be2c2235d969cb161fcb3b5de874
Andrey Melnikov <a.melnikov1310@ya.ru>
2020-02-26 17:45:36 +0300

    Домашка 5, мишшен комплит

--- src/HomeWork5/Employee.java
+++ src/HomeWork5/Employee.java
@@ -9,20 +9,33 @@ public class Employee {
     String phoneNumber;
     int age;
 
-// третий пункт ДЗ
+    public Employee() { // чтобы второй пункт задания не краснел))
+    }
+
+    // третий пункт ДЗ
     public void info() {
         System.out.printf("ФИО сотрудника: %s\nЗанимаемая должность: %s с окладом в %s\nВозраст сотрудника: %s лет" +
                         "\nКонтактные данные:\nНомер телефона: %s\nЭлектронная почта: %s",
                 name, position, salary, age, phoneNumber, email);
         System.out.println();
     }
+
     // конструктор для массива
-    public Employee(String name, String position, int salary, int age, String phoneNumber, String email){
+    public Employee(String name, String position, int salary, int age, String phoneNumber, String email) {
         this.name = name;
         this.position = position;
         this.salary = salary;
         this.age = age;
         this.phoneNumber = phoneNumber;
         this.email = email;
+    }
+    // переопределение ТУСТРИНГ ))))
+    @Override
+    public String toString() {
+        return name + " | " + position + " | " + salary + " | " + email + " | " + phoneNumber + " | " + age;
+    }
+
+    public int getAge() {
+        return age;
     }
 }
